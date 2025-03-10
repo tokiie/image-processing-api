9e3714d7bbb2ea3a01471084ef093dbb817258ce
tokiie <yassine.math@gmail.com>
2025-03-10 12:18:16 +0900

    feat: Initialize project setup - Add package.json with core dependencies - Configure TypeScript - Set up Jest - Add README

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  }
+};
